0c21525f42520c10e324b039b3ab0e3f4fbc14e0
Pedroh1510 <47982475+Pedroh1510@users.noreply.github.com>
2026-02-01 19:11:46 -0300

    feat: apply `authorization.filterOutput()` to all endpoints

--- models/authorization.js
+++ models/authorization.js
@@ -12,8 +12,21 @@ function can(user, feature, resource) {
   return authorized;
 }
 
+function filterOutput(user, feature, output) {
+  if (feature === "read:user") {
+    return {
+      id: output.id,
+      username: output.username,
+      created_at: output.created_at,
+      updated_at: output.updated_at,
+      features: output.features,
+    };
+  }
+}
+
 const authorization = {
   can,
+  filterOutput,
 };
 
 export default authorization;
