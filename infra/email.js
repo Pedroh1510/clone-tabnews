d61025efe8efbee9c708775aa119f44cfbeb19d8
Pedroh1510 <47982475+Pedroh1510@users.noreply.github.com>
2026-03-27 14:39:49 -0300

    chore: add env para habilitar o secure no email

--- infra/email.js
+++ infra/email.js
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_SMTP_HOST,
   port: process.env.EMAIL_SMTP_PORT,
-  secure: false,
+  secure: process.env.EMAIL_SMTP_SECURE === "true",
   auth: {
     user: process.env.EMAIL_SMTP_USER,
     pass: process.env.EMAIL_SMTP_PASS,
